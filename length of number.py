425291c4a97ba3be41b26a670905a51767809717
CHUNG-WEI-YONG <chungwyong@gmail.com>
2025-06-12 20:18:12 +0800

    Add files via upload

---
+++ length of number.py
@@ -0,0 +1,13 @@
+def length():
+    number = str(input("Number:"))
+    list1 = list(number)
+    a = 0
+    for i in list1:
+        a+=1
+    print("Input: %d \nOutput: %d"%(int(number),a))
+length()
+        
+
+
+        
+     
