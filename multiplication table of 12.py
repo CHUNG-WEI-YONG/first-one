2226b08d7dd7b330f84a400926f3cd36d31eac01
CHUNG-WEI-YONG <chungwyong@gmail.com>
2025-06-12 20:58:56 +0800

    Add files via upload
    Ask user for a number. Print its multiplication table from 1 to 12.

---
+++ multiplication table of 12.py
@@ -0,0 +1,12 @@
+def multiplication_number() :
+    number = int(input("Number chosen :"))
+    for i in range(1,13):
+        answer  = number*i
+        print("%d x %d = %d"%(number , i , answer))
+multiplication_number()
+        
+    
+
+
+        
+     
